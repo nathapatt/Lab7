943fe029b624b4169e999d0763e273d0e9482db3
Mind <nathapat.mind@gmail.com>
2022-12-13 17:32:51 +0700

    Lab7_Final4

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -2,6 +2,8 @@
 #include<string>
 
 using namespace std;
+double X;
+int i = 0;
 
 int main(){
 	
@@ -9,7 +11,17 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
-		
+    cout << "Please input GPA: ";
+    cin >> X;
+    
+    while(i<10){
+        if(gpa[i] >= X){
+            
+            cout << id[i] << " " << name[i] << " " << gpa[i] << "\n";
+            
+        }
+        i++;
+    }
+    
 	return 0;
 }
